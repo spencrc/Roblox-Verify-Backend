0251eca9460d61c57ef22bc896b19d960d70f3d4
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-26 01:01:55 -0700

    added path to views

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,18 @@ import indexRoutes from './routes/index.js';
 import redirectRoutes from './routes/redirect.js';
 import termsRoute from './routes/terms.js';
 import errorMiddleware from './middleware/error-middleware.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Start Express.js server!
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.set('views', `${__dirname}/../views`);
 app.use(express.static('public'));
 
 app.use('/', indexRoutes);
