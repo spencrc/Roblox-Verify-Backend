5ced7b9a11b205818997b8c81c3098ea18cf2f68
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-26 01:50:55 -0700

    implemented layouts for easier setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { PORT } from './config.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 import express from 'express';
 import indexRoutes from './routes/index.js';
 import redirectRoutes from './routes/redirect.js';
 import termsRoute from './routes/terms.js';
 import errorMiddleware from './middleware/error-middleware.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import expressLayouts from 'express-ejs-layouts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,8 +16,9 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.set('views', `${__dirname}/../views`);
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 app.use(express.static(`${__dirname}/../public`));
 
 app.use('/', indexRoutes);
