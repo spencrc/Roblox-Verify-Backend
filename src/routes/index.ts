cf5b9a3823fb9a44b286414e5459351f6c06ae03
spencerishere <145073726+spencerishere@users.noreply.github.com>
2025-07-31 09:32:18 -0700

    index now displays success message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,5 @@ const router = Router();
 
 export default router.get('/', (req, res) => {
 	//res.send('Welcome to the Roblox Verification API! This is not a website, but an API.');
-	res.render("index", { text: "World"});
+	res.render("index");
 });
